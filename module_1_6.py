b90073902b462ac0243ae41a0c85be552c4d7131
luchik087 <mailtupka@gmail.com>
2024-11-19 21:11:59 +0500

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+home_work_count = 12
+hour_count = 1.5
+course_name = 'Python'
+time_to_complete_1_task = hour_count/home_work_count
+print('Курс: ',course_name,',', 'всего задач:', home_work_count,',', 'затрачено часов: ',hour_count,', среднее время выполнения ',time_to_complete_1_task,' часа' )
