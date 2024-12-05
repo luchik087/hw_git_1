35ac84c660375331b1bbd84389e6daf8908fa733
luchik087 <mailtupka@gmail.com>
2024-12-06 01:23:17 +0500

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -1,5 +1,6 @@
 def send_email(message, recipient, sender="university.help@gmail.com", ):
-    if "@" not in (recipient or sender) or not recipient.endswith((".com", ".ru", ".net")) or not sender.endswith(
+    if "@" not in recipient or "@" not in sender or not recipient.endswith(
+            (".com", ".ru", ".net")) or not sender.endswith(
             (".com", ".ru", ".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}.")
     elif sender == recipient:
